45049a5f317e4233d29270807463716fc08be61d
Harshit Shrivastava <harshit.s@quickinsure.co.in>
2024-03-14 11:51:59 +0530

    signup and signout fuctionality added

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/meeting/:id*"],
-  // ignoredRoutes: ["/no-auth-in-this-route"],
 });
 
 export const config = {
